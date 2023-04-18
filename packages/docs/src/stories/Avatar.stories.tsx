1d466dc3ee732a47175950b8fd35a0ac156c288f
Nicolas Segatto <nicolasssegatto@icloud.com>
2023-04-17 21:55:13 -0300

    up corrigido

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/nicolassegatto.png',
     alt: 'Avatar image',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
